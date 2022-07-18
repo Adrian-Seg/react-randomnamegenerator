677e42548ba83a9b2ec67f5b8f056c542bb1f14f
Adrian-Seg <asegura1306@yahoo.com>
2022-07-18 09:55:05 -0700

    removing names from array

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,28 @@
 import { useState } from "react"
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Button } from "react-bootstrap"
 
 const MainPage = () => {
     const [addedName, setAddedName] = useState('');
     const [list, setList] = useState([]);
 
+    // Binding text input to variable
     const handleChange = (e) => {
         setAddedName(e);
+    }
+    const logArray = (e) => {
+        console.log(list);
+    }
+    // Binding text variable to array and resetting text input
+    const handleClick = () => {
+        setList([...list, addedName]);
+        setAddedName('');
+    }
+    // Removing selected element from array
+    const handleRemove = (index) => {
+        setList([
+        ...list.slice(0, index),
+        ...list.slice(index + 1, list.length
+        )]);
     }
 
     return (
@@ -15,8 +31,19 @@ const MainPage = () => {
                 <Col className="center">
                     <label>Name: </label>
                     <input type="text"  onChange={ (e) => handleChange(e.target.value)} value={addedName}></input>
+                    <Button onClick={handleClick}>Submit</Button>
                 </Col>
             </Row>
+            <Row>
+                <Col>
+                {
+                    list.map((name, index) => {
+                        return <h1 key={index} onClick={() => handleRemove(index)} >{name} - {index}</h1>
+                    })
+                }                
+                </Col>
+            </Row>
+            <Button onClick={logArray}>Log array</Button>
         </Container>
     )
 }
